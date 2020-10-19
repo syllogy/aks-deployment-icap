7f2baea962ac7c8498e942752a6ddb35942fe0cb
Matthew Pigram <mpigram@glasswallsolutions.com>
2020-10-19 10:33:51 +0100

    removed modules that aren't needed

--- modules/network/main.tf
+++
@@ -1,7 +0,0 @@
-# Create virtual network
-resource "azurerm_virtual_network" "net_virtual_network" {
-  name                = var.service_name
-  address_space       = var.address_space
-  location            = var.region
-  resource_group_name = var.resource_group
-}
--- modules/network/output.tf
+++
@@ -1,8 +0,0 @@
-output "service_name" {
-	value = "${azurerm_virtual_network.net_virtual_network.name}"
-}
-
-output "address_space" {
-	value = "${azurerm_virtual_network.net_virtual_network.address_space}"
-}
-
