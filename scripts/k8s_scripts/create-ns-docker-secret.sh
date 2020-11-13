add7573eb6a2b13597623c6b9eefcea4c4ce52aa
Matthew Pigram <mpigram@glasswallsolutions.com>
2020-11-13 16:39:59 +0000

    updated scripts to create more secrets

--- scripts/k8s_scripts/create-ns-docker-secret.sh
+++ scripts/k8s_scripts/create-ns-docker-secret.sh
@@ -5,15 +5,20 @@
 # Variables
 DOCKER_SERVER="https://index.docker.io/v1/"
 USER_EMAIL="mpigram@glasswallsolutions.com"
-DOCKER_USERNAME=$(az keyvault secret show --name DH-SA-USERNAME --vault-name gw-tfstate-vault-eastus --query value -o tsv)
-DOCKER_PASSWORD=$(az keyvault secret show --name DH-SA-password --vault-name gw-tfstate-vault-eastus --query value -o tsv)
-FILESHARE_ACCOUNT_NAME=$(az keyvault secret show --name file-share-account --vault-name gw-tfstate-vault-eastus --query value -o tsv)
-FILESHARE_KEY=$(az keyvault secret show --name file-share-key --vault-name gw-tfstate-vault-eastus --query value -o tsv)
+DOCKER_USERNAME=$(az keyvault secret show --name DH-SA-USERNAME --vault-name gw-tfstate-vault-13222 --query value -o tsv)
+DOCKER_PASSWORD=$(az keyvault secret show --name DH-SA-password --vault-name gw-tfstate-vault-13222 --query value -o tsv)
+FILESHARE_ACCOUNT_NAME=$(az keyvault secret show --name file-share-account --vault-name gw-tfstate-vault-13222 --query value -o tsv)
+FILESHARE_KEY=$(az keyvault secret show --name file-share-key --vault-name gw-tfstate-vault-13222 --query value -o tsv)
+TOKEN_USERNAME=$(az keyvault secret show --name token_username --vault-name gw-tfstate-vault-13222 --query value -o tsv)
+TOKEN_PASSWORD=$(az keyvault secret show --name token_password --vault-name gw-tfstate-vault-13222 --query value -o tsv)
+
+# Namspaces
 NAMESPACE01="icap-adaptation"
 NAMESPACE02="management-ui"
 NAMESPACE03="transaction-event-api"
 NAMESPACE04="prometheus-stack"
-NAMESPACE05="rabbitmq-controller"
+# NAMESPACE05="rabbitmq-controller"
+NAMESPACE06="policy-management-api"
 
 # Create namespaces for deployment
 kubectl create ns $NAMESPACE01
@@ -32,5 +37,11 @@ kubectl create -n $NAMESPACE01 secret docker-registry regcred \
 # Create secret for file share - needs to be part of the 'transaction-event-api' namespace
 kubectl create -n $NAMESPACE03 secret generic transactionstoresecret --from-literal=accountName=$FILESHARE_ACCOUNT_NAME --from-literal=accountKey=$FILESHARE_KEY
 
+# Create secret for file share - needs to be part of the 'transaction-event-api' namespace
+kubectl create -n $NAMESPACE04 secret generic policystoresecret --from-literal=accountName=$FILESHARE_ACCOUNT_NAME --from-literal=accountKey=$FILESHARE_KEY 
+kubectl create -n $NAMESPACE04 secret generic policyUpdateServiceRef --from-literal=accountName=$FILESHARE_ACCOUNT_NAME --from-literal=accountKey=$FILESHARE_KEY --from-literal=TokenUsername=$TOKEN_USERNAME --from-literal=TokenPassword=$TOKEN_PASSWORD
+
+
 # Create secret for TLS certs & keys - needs to be part of the 'icap-adaptation' namespace
 kubectl create -n $NAMESPACE01 secret tls icap-service-tls-config --key tls.key --cert certificate.crt
+
