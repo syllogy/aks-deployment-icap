1251a89b2a2a1b7f50d632e8be803a8e1f551e02
Matthew Pigram <69460780+mpigram@users.noreply.github.com>
2021-02-08 19:59:43 +0000

    created scripts for new file-drop clusters and updated argocdscript

---
+++ scripts/k8s_scripts/uk-south/file-drop-secrets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+kubectl config use-context gw-icap-uks-file-drop-develop
+
+NAMESPACE01="icap-filedrop"
+
+kubectl create ns $NAMESPACE01
+
+# Create secret for TLS certs & keys - needs to be part of the 'icap-filedrop' namespace
+(cd ./certs/filedrop-cert; kubectl create -n $NAMESPACE01 secret tls tls-secret --key tls.key --cert certificate.crt)
